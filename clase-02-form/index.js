17381d6a36b7cc934a7765184db393ed56be3ad6
LeooPaiz <leandropaiz94@gmail.com>
2025-10-31 21:04:32 -0300

    guarda los cambios

---
+++ clase-02-form/index.js
@@ -0,0 +1,8 @@
+
+//esto es un comentario de una sola línea
+var nombre; //declaración de variable
+nombre = "Juan"; //asignación de valor
+
+nombre = "Pedro"; //reasignación de valor
+
+nombre = 123; //reasignación de valor con diferente tipo de dato
