d204a2cc6c6f8b06fee09d653de8f93a892e8c56
LeooPaiz <leandropaiz94@gmail.com>
2025-11-05 21:29:12 -0300

    agregamos nuestro primer archivo de JS

--- clase-02-form/index.js
+++ clase-02-form/index.js
@@ -2,7 +2,22 @@
 //esto es un comentario de una sola línea
 var nombre; //declaración de variable
 nombre = "Juan"; //asignación de valor
-
+nombre = 123;
 nombre = "Pedro"; //reasignación de valor
 
-nombre = 123; //reasignación de valor con diferente tipo de dato
+ //reasignación de valor con diferente tipo de dato
+var edad = 30; //declaración y asignación en una sola línea
+
+console.log(nombre); //imprime el valor de la variable en la consola
+console.log(edad);
+
+var ciudad= "Buenos Aires"; //declaración y asignación de variable
+var pais = "Argentina"; //declaración y asignación de variable
+var continente = "América"; //declaración y asignación de variable
+var dni= 12345678; //declaración y asignación de variable
+var soltero= true; //declaración y asignación de variable booleana
+var tieneMascota= false; //declaración y asignación de variable booleana
+    
+console.log(ciudad,pais,continente,dni,soltero,tieneMascota);
+ var marcaAuto= "Toyota"; //declaración y asignación de variable
+ var modeloAuto= "Corolla";
