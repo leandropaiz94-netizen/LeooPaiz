7dbf9d9a0224aa6c49d863b7543e26ab31fc5fe2
LeooPaiz <leandropaiz94@gmail.com>
2025-11-07 21:04:34 -0300

    creamos constantes

--- clase-02-form/index.js
+++ clase-02-form/index.js
@@ -11,13 +11,36 @@ var edad = 30; //declaración y asignación en una sola línea
 console.log(nombre); //imprime el valor de la variable en la consola
 console.log(edad);
 
-var ciudad= "Buenos Aires"; //declaración y asignación de variable
-var pais = "Argentina"; //declaración y asignación de variable
+var ciudad= "Buenos Aires";
+var pais = "Argentina";
 var continente = "América"; //declaración y asignación de variable
-var dni= 12345678; //declaración y asignación de variable
+var dni= 12345678; 
 var soltero= true; //declaración y asignación de variable booleana
 var tieneMascota= false; //declaración y asignación de variable booleana
     
 console.log(ciudad,pais,continente,dni,soltero,tieneMascota);
- var marcaAuto= "Toyota"; //declaración y asignación de variable
+ var marcaAuto= "Toyota";
  var modeloAuto= "Corolla";
+ var fruta, color, sabor;
+ console.log(fruta);
+ // no ejecuta datos del front
+ var localidad;
+ var CUENTA_BANCARIA;
+ var paisDeOrigen;
+ var palabraReservada = "esto no se debe hacer"; //no usar palabras reservadas como nombres de variables
+
+ //constantes
+ const IVA = 0.21; //declaración de constante
+ const Brasil= "pais limita con Argentina"; //declaración de constante
+
+ console.log("el valor del IVA es: " + IVA);
+ console.log("un pais limitro con Argentina es: " + Brasil);
+
+
+    // sumas
+    let numerouno = 10;
+    let numerodos = 5;
+    console.log("la suma de  "+numerouno+ " + "+numerodos+" es igual a: " + (numerouno + numerodos));
+    console.log(
+        "texto plano que quiero mostrar:" +  Brasil + pais
+    )
